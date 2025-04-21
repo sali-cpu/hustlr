3eff88f69c7734bb9cbb2689cbe356d68824d915
sali-cpu <mbaye.salimata@icloud.com>
2025-04-21 18:17:16 +0200

    Add files via upload

---
+++ src/components/FooterClient.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../stylesheets/Final.css';
+
+const FooterClient = () => {
+  return (
+    <footer style={{ backgroundColor: "#f8fafc", textAlign: "center", padding: "1rem", marginTop: "auto" }}>
+      <p>&copy; 2025 YourPlatform. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default FooterClient;
