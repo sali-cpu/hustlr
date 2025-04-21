3117b4618d3708849021c912fb597fa1b6172c69
sali-cpu <mbaye.salimata@icloud.com>
2025-04-21 18:48:50 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import SignUp from "./pages/SignUp";
 import Client from "./pages/Client";
 import Freelancers from "./pages/Freelancer";
 import AdminCorrect from "./pages/AdminCorrect";
+import ClientJobs from "./pages/ClientJobs"; 
+import ClientPayments from "./pages/ClientPayments";
+import FreelancerJobs from "./pages/FreelancerJobs";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -29,11 +32,14 @@ function App() {
               </>
             }
           />
-          <Route path="/SignUp" element={<SignUp />} />
+           <Route path="/SignUp" element={<SignUp />} />
           <Route path="/Land" element={<Land />} />
           <Route path="/Client" element={<Client />} />
           <Route path="/Freelancer" element={<Freelancers />} />
           <Route path="/Admin" element={<AdminCorrect />} />
+          <Route path="/ClientJobs" element={<ClientJobs />} />
+          <Route path="/ClientPayments" element={<ClientPayments />} />
+          <Route path="/FreelancerJobs" element={<FreelancerJobs />} />
         </Routes>
       </section>
       
