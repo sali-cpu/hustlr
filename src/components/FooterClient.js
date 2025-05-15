d93459755bbbc2d2ec0e8847d548dbdd07775be8
WakkyCodes <wakky47@hotmail.com>
2025-05-15 17:27:34 +0200

    Update FooterClient.js

--- src/components/FooterClient.js
+++ src/components/FooterClient.js
@@ -4,7 +4,7 @@ import '../stylesheets/Final.css';
 const FooterClient = () => {
   return (
     <footer style={{ backgroundColor: "#f8fafc", textAlign: "center", padding: "1rem", marginTop: "auto" }}>
-      <p>&copy; 2025 YourPlatform. All rights reserved.</p>
+      <p>&copy; 2025 Hustlr. All rights reserved.</p>
     </footer>
   );
 };
