11065f5ef3643e3c514940d8cc8bd6041ada8ebe
Sesihle-Goniwe <sesihleg1@gmail.com>
2025-05-16 00:18:29 +0200

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,22 @@
 // jest.config.js
 module.exports = {
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  setupFilesAfterEnv: [
+    '<rootDir>/src/setupTests.js',
+    '<rootDir>/jest.setup.js',
+  ],
   transform: {
     '^.+\\.[jt]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
   testEnvironment: 'jsdom',
+
+  // ✅ Coverage config here
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx}",
+    "!**/node_modules/**",
+    "!**/vendor/**",
+    "!**/*.test.js",
+  ],
+  coverageDirectory: "coverage",
 };
